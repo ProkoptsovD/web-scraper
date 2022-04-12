679a63fbc2ef0f43dacea03169c31f94da52ce94
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-04-12 22:56:42 +0300

    some attempts

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const cors = require('cors')({ origin: true });
-const cheerio = require('cheerio');
-const fetch = require('node-fetch');
-const getUrls = require('get-urls');
+import cors from 'cors';
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import getUrls from 'get-urls';
+
+const response = await fetch('https://www.zieglers.com/');
+const body = await response.text();
+const $ = cheerio.load(body);
+
+const r = [];
+$('[data-collapsible]').each((i, elm) => r.push($(elm).attr('href')));
+
+const res1 = await Promise.all(
+	r.map(async (link) => await (await fetch(link)).text())
+);
+const parsedResp = res1.map((tex) => {
+	const $ = cheerio.load(tex);
+	const r2 = [];
+	$('figure > a').each((i, elm) => r2.push($(elm).attr('href')));
+
+	return r2;
+});
+console.log(parsedResp);
